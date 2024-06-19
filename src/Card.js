e8d09748146abf58c198dfe4021cd59d7c90581a
Inchara <108918460+IncharaP@users.noreply.github.com>
2024-06-19 13:48:29 +0530

    Update Card.js

--- src/Card.js
+++ src/Card.js
@@ -1,24 +1,23 @@
-import React from "react";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Card = (movie) => {
+const Card = ({ info }) => {
   let img_path = "https://image.tmdb.org/t/p/w500";
+
   return (
-    <>
-      <div className="movie">
-        <img src={img_path + movie.info.poster_path} alt={movie.info.title || ""} className="poster"></img>
-        <div className="movie-details">
-          <div className="box">
-            <h4 className="title">{movie.info.title}</h4>
-            <p className="rating">{movie.info.vote_average}</p>
-          </div>
-          <div className="overview">
-            <h1>Overview</h1>
-            {movie.info.overview}
-          </div>
+    <div className="movie">
+      <Link to={`/movie/${info.id}`}>
+        <img src={img_path + info.poster_path} alt={info.title || ""} className="poster" />
+      </Link>
+      <div className="movie-details">
+        <div className="box">
+          <h4 className="title">{info.title}</h4>
+         
         </div>
+        
       </div>
-    </>
-  )
-}
+    </div>
+  );
+};
 
 export default Card;
